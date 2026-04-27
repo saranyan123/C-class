030866af35c8fa37261cdc5092df09457c3fad7d
SARANYAN V <150230865+saranyan123@users.noreply.github.com>
2026-04-27 20:51:01 +0530

    Time: 1 ms (11.83%) | Memory: 43.3 MB (15.50%) - LeetSync

---
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> result = new LinkedList<>();
+        if (root == null) return result;
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            // addFirst essentially reverses the order: 
+            // Result becomes Root -> Right -> Left, but viewed backwards is Left -> Right -> Root
+            result.addFirst(node.val);
+
+            // Push Left first so that Right is processed first (opposite of Preorder)
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+        }
+
+        return result;
+    }
+}
